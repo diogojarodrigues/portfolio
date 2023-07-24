6fea22f5f22e29e497f6e8f7e6a51349138540b6
diogojarodrigues <diogojarodrigues@outlook.pt>
2023-07-24 18:43:16 +0100

    animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,56 @@
 import { Stack, Image, Button, Row, Col } from "react-bootstrap";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { motion } from "framer-motion";
 
 import profilePicture from "../assets/profile-picture.jpeg";
 
-export default function Hero() {
+export default function Hero({ isMobile }) {
   return (
     <div className="hero" id="hero">
-      <Image src={profilePicture} alt="Profile Picture" roundedCircle style={{ width: "20rem" }} />
+      <motion.div
+        initial={{ opacity: 0, y: 0 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 3 }}
+      >
+        <Image src={profilePicture} alt="Profile Picture" roundedCircle style={{ width: "20rem" }} />
+      </motion.div>
 
-      <h1 style={{ marginBottom: "1.2rem", marginTop: "2.2rem" }} className="h1-responsive text-center">Diogo Rodrigues</h1>
-      <h4 style={{ marginBottom: "5rem"}}>Student, Developer & Self Learner</h4>
+      {/* Apply the animation to the header */}
+      <motion.h1
+        style={{ marginBottom: "1.2rem", marginTop: "2.2rem" }}
+        className="h1-responsive text-center"
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, delay: 1 }}
+      >
+        Diogo Rodrigues
+      </motion.h1>
+      <motion.h4
+        style={{ marginBottom: "5rem" }}
+        className="text-center"
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, delay: 1.5 }}
+      >
+        Student, Developer & Self Learner
+      </motion.h4>
 
-      <Stack direction="horizontal" gap={3} className="mx-auto">
-            <Button className="icon-link" target="blank" href="https://www.linkedin.com/in/diogo-rodrigues-7181a0246/" variant="light">
-                <BsLinkedin />
-                LinkedIn
-            </Button>
-            <Button className="icon-link" target="blank" href="https://github.com/diogojarodrigues" variant="light">
-                <BsGithub />
-                Github
-            </Button>
-      </Stack>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 2.4 }}
+      >
+        <Stack direction="horizontal" gap={3} className="mx-auto">
+          <Button className="icon-link" target="_blank" href="https://www.linkedin.com/in/diogo-rodrigues-7181a0246/" variant="light">
+            <BsLinkedin />
+            LinkedIn
+          </Button>
+          <Button className="icon-link" target="_blank" href="https://github.com/diogojarodrigues" variant="light">
+            <BsGithub />
+            Github
+          </Button>
+        </Stack>
+      </motion.div>
     </div>
   );
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,11 @@
 import { Container, Row, Col, Image } from "react-bootstrap";
 import { useMediaQuery } from 'react-responsive';
+import { motion, useInView, useAnimation } from "framer-motion";
 
 import pomodoroPulse from "../assets/pomodoropulse(retangulo).png";
 import webstore from "../assets/webstore(retangulo).png";
 import solveETH from "../assets/SolveETH(retangulo).png";
+import { useEffect, useRef } from "react";
 
 
 const projects = [
@@ -33,36 +35,159 @@ const projects = [
     },
 ]
 
+const getVariants = (isMobile, index, type ) => {
+    const left = {
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 },
+    }
+
+    const right = {
+        hidden: { opacity: 0, x: 100 },
+        visible: { opacity: 1, x: 0 },
+    }
+
+    const middle = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+    }
+
+    let variant
+
+    if (isMobile === true) {
+
+        if (index % 2 === 0) {
+            switch (type) {
+                case "title": variant = left; break;
+                case "image": variant = right; break;
+                case "text": variant = middle; break;
+            }
+        } else {
+            switch (type) {
+                case "title": variant = right; break;
+                case "image": variant = left; break;
+                case "text": variant = middle; break;
+            }
+        }
+
+    } else {
+
+        if (index % 2 === 0) {
+            switch (type) {
+                case "image": variant = left; break;
+                case "title": variant = right; break;
+                case "text": variant = middle; break;
+            }
+        } else {
+            switch (type) {
+                case "image": variant = right; break;
+                case "title": variant = left; break;
+                case "text": variant = middle; break;
+            }
+        }
+    }
+    return variant
+}
+
+
 export default function Portfolio({isMobile}) {
 
     const renderProjects = (isMobile, shortDescription) => {
         return projects.map((project, index) => {
-            
-            const ImageComponent = () => {
+
+            const TitleAnimated = () => {
+                const titleRef = useRef(null);
+                const isTitleVisible = useInView(titleRef, { once: true, amount: 0.4 });
+                const titleAnimation = useAnimation();
+
+                useEffect(() => {
+                    if (isTitleVisible) {
+                        titleAnimation.start("visible");
+                    }
+                }, [isTitleVisible]);
+
                 return (
-                    <Col lg={6} className="px-3">
-                    <a href={project.github} target="_blank" rel="noopener noreferrer">
-                        <div className="image-container">
-                        <Image
-                            src={project.image}
-                            alt={project.title}
-                            fluid
-                            style={{ borderRadius: "2rem", boxShadow: '0px 0px 15px 3px rgba(0, 0, 0, 0.3)' }}
-                        />
-                        </div>
-                    </a>
-                    </Col>
+                    <motion.h1
+                        ref={titleRef} 
+                        className="text-center mb-4"
+                        variants={getVariants(isMobile, index, "title")}
+                        initial="hidden"
+                        animate={titleAnimation}
+                        transition={{ duration: 1.3 }}
+                    >
+                        {project.title}
+                    </motion.h1>
+                )
+            }
+
+            const TextAnimated = () => {
+                const textRef = useRef(null);
+                const isTextVisible = useInView(textRef, { once: true, amount: 0.7 });
+                const textAnimation = useAnimation();
+
+                useEffect(() => {
+                    if (isTextVisible) {
+                        textAnimation.start("visible");
+                    }
+                }, [isTextVisible]);
+
+                return (
+                    <motion.p
+                        ref={textRef} 
+                        className="text-center px-3 mt-4 mb-3"
+                        variants={getVariants(isMobile, index, "text")}
+                        initial="hidden"
+                        animate={textAnimation}
+                        transition={{ duration: 2 }}
+                    >
+                        {(shortDescription || isMobile) ? project.shortDescription : project.description}
+                    </motion.p>
+                )
+            }
+
+            const ImageAnimated = () => {
+                const imageRef = useRef(null);
+                const isImageVisible = useInView(imageRef, { once: true, amount: 0.6 });
+                const imageAnimation = useAnimation();
+
+                useEffect(() => {
+                    if (isImageVisible) {
+                        imageAnimation.start("visible");
+                    }
+                }, [isImageVisible]);
+
+                return (
+                        <a href={project.github} target="_blank" rel="noopener noreferrer">   
+                            <motion.div 
+                                className="image-container mx-2"
+                                ref={imageRef}
+                                variants={getVariants(isMobile, index, "image")}
+                                initial="hidden"
+                                animate={imageAnimation}
+                                transition={{ type: 'spring', duration: 2, bounce: 0.6 }}
+                            >
+                                <Image
+                                    src={project.image}
+                                    alt={project.title}
+                                    fluid
+                                    style={{ borderRadius: "2rem", boxShadow: '0px 0px 15px 3px rgba(0, 0, 0, 0.3)' }}
+                                />
+                            </motion.div>
+                        </a>
+                        // </motion.div>
                 )
             }
 
             if (isMobile === true) {
+
                 return (
                     <Container className="pb-5 mb-4" key={project.title} >
-                    <Row className="d-flex flex-column justify-items-center">
-                        <h1 className="text-center mb-4">{project.title}</h1>
-                        <ImageComponent />
-                        <p className="text-center px-3 mt-4 mb-3">{project.shortDescription}</p>
-                        {project.link && <a href={project.link} style={{width: "auto"}} className="btn btn-primary my-3" target="_blank" rel="noopener noreferrer">Visit Website</a>}
+                    <Row>
+                        <Col lg={6} className="d-flex flex-column justify-content-center align-items-center px-0">
+                            <TitleAnimated />
+                            <ImageAnimated />
+                            <TextAnimated />
+                            {project.link && <a href={project.link} className="btn btn-primary my-3" target="_blank" rel="noopener noreferrer">Visit Website</a>}
+                        </Col>
                     </Row>
                     </Container>
                 ) 
@@ -84,13 +209,23 @@ export default function Portfolio({isMobile}) {
                 <Row>
                     {index % 2 === 0 ? (
                         <>
-                        <ImageComponent />
-                        <TextComponent />
+                            <Col>
+                                <ImageAnimated />
+                            </Col>
+                            <Col lg={6} className="d-flex flex-column justify-content-center align-items-center px-5">
+                                <TitleAnimated />
+                                <TextAnimated />
+                            </Col>
                         </>
                     ) : (
                         <>
-                        <TextComponent />
-                        <ImageComponent />
+                            <Col lg={6} className="d-flex flex-column justify-content-center align-items-center px-5">
+                                <TitleAnimated />
+                                <TextAnimated />
+                            </Col>
+                            <Col>
+                                <ImageAnimated />
+                            </Col>
                         </>
                     )}
                 </Row>
